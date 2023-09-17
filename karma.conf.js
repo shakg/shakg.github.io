273f2943fd4d7dfbb32d8086c376e3d326cb3b94
İshak Gönül <ishak.gonul@titra.com.tr>
2023-09-17 14:38:52 +0300

    chore: remove old project and add new personal project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/otomatik-mama-kabi'),
+      dir: require('path').join(__dirname, './coverage/shakg-dev'),
       subdir: '.',
       reporters: [
         { type: 'html' },
