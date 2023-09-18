2ad58c8dfe2a20f5cfab10773e59b97c24157b8a
İshak Gönül <ishak.gonul@titra.com.tr>
2023-09-18 10:04:48 +0300

    feat: add links to social contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,4 +16,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
